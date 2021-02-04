fe05d217578dd7001b3e95a8386727c2f2d0c156
David-Martinex <franciscotvgamer@gmail.com>
2021-02-04 16:21:20 -0600

    terminando de documentar DataBase

--- proyecto/DataBase.c
+++ proyecto/DataBase.c
@@ -1,3 +1,12 @@
+/**
+ * @file DataBase.c
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "DataBase.h"
 
 /**=============================================================== 
--- proyecto/DataBase.h
+++ proyecto/DataBase.h
@@ -1,15 +1,38 @@
 #ifndef  DataBase_INC
 #define  DataBase_INC
+/**
+ * @file DataBase.h
+ * @brief Se encargara de guardar nuestra estructura que compone a nuestro Tipo
+ * abtsracto DataBase, tambien los prototipos de los metodos relacionados con
+ * este Tipo Abstracto.  
+ * @version 0.1
+ * @date 2021-02-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/** para usar bool´s*/
 #include <stdbool.h>
+/** Para usar assert´s*/
 #include <assert.h>
+/** Para usar strcpy y strcmp */
 #include <string.h>
 
+/**Para usar a Station y algunos de metodos relaionados*/
 #include "Station.h"
+/**Para usar a Roots y algunos de metodos relaionados*/
 #include "Roots.h"
+/**Para hacer uso de nuestras bases de datos SQL y algunos metodos*/
 #include "sqlite3.h"
+/**Para usar a DLL y algunos de sus metodos relaionados*/
 #include "DLL.h"
 
+/**
+ * @brief Declaramos un tipo abstracto denominado DataBase que contendra 
+ * entre sus atributos un sqlite3 para hacer uso de base de datos SQL.
+ * 
+ */
 typedef struct DataBase
 {
 	sqlite3* dataBase;
